2efbd5251716eccc03bedec67f2e4e7053eefdd7
Nils Lien <nils.lien@dsb.no>
2019-05-14 12:08:28 +0200

    #18: Update doc with example application

---
+++ examples/hello-keycloak/src/main.js
@@ -0,0 +1,26 @@
+import VueKeycloakJs from '@dsb-norge/vue-keycloak-js'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+Vue.use(VueKeycloakJs, {
+  init: {
+    // Use 'login-required' to always require authentication
+    // If using 'login-required', there is no need for the router guards in router.js
+    onLoad: 'check-sso'
+  },
+  config: {
+    url: 'https://mykeycloak-server.com/auth',
+    clientId: 'MyClientId',
+    realm: 'MyRealm'
+  },
+  onReady: (keycloak) => {
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
---
+++ examples/hello-keycloak/src/views/Secret.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>This is a secret about page. You should only be able to see this if you are authenticated.</h1>
+    <h3>This is what my token looks like:</h3>
+    <code>{{ $keycloak.tokenParsed }}</code>
+  </div>
+</template>
