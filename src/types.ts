7e48de3f03c2b15c3f24aa172df15ccf7bc0bf86
shenhaoliang <rolandshen@etop.com>
2021-03-31 11:25:46 +0800

    refactor code to ts and vue3

---
+++ src/types.ts
@@ -0,0 +1,73 @@
+import type {
+  KeycloakConfig,
+  KeycloakError,
+  KeycloakInitOptions,
+  KeycloakInstance,
+  KeycloakLoginOptions,
+  KeycloakProfile,
+  KeycloakPromise,
+  KeycloakResourceAccess,
+  KeycloakRoles,
+  KeycloakTokenParsed,
+} from "keycloak-js";
+
+export { KeycloakConfig, KeycloakError } from "keycloak-js";
+
+export type AnyFunction = (...args: any[]) => any;
+
+export interface KConfig extends KeycloakConfig {
+  authRealm?: string;
+  authUrl?: string;
+  authClientId?: string;
+  logoutRedirectUri?: string;
+}
+
+export type VueKeycloakConfig = KConfig | string;
+
+export interface VueKeycloakOptions {
+  config?: VueKeycloakConfig;
+  init?: KeycloakInitOptions;
+  logout?: any;
+  onReady?: (keycloak: KeycloakInstance) => void;
+  onInitError?: (error: Error, err: KeycloakError) => void;
+  onAuthRefreshError?: (keycloak: KeycloakInstance) => void;
+  onInitSuccess?(authenticated: boolean): void;
+}
+
+export interface VueKeycloakTokenParsed extends KeycloakTokenParsed {
+  preferred_username?: string;
+  name?: string;
+}
+
+export interface VueKeycloakInstance {
+  ready: boolean; // Flag indicating whether Keycloak has initialised and is ready
+  authenticated: boolean;
+  userName?: string; // Username from Keycloak. Collected from tokenParsed['preferred_username']
+  fullName?: string; // Full name from Keycloak. Collected from tokenParsed['name']
+  login?(options?: KeycloakLoginOptions): KeycloakPromise<void, void>; // [Keycloak] login function
+  loginFn?(options?: KeycloakLoginOptions): KeycloakPromise<void, void>; // Alias for login
+  logoutFn?(options?: any): KeycloakPromise<void, void> | void; // Keycloak logout function
+  createLoginUrl?(options?: KeycloakLoginOptions): string; // Keycloak createLoginUrl function
+  createLogoutUrl?(options?: any): string; // Keycloak createLogoutUrl function
+  createRegisterUrl?(options?: KeycloakLoginOptions): string; // Keycloak createRegisterUrl function
+  register?(options?: KeycloakLoginOptions): KeycloakPromise<void, void>; // Keycloak register function
+  accountManagement?(): KeycloakPromise<void, void>; // Keycloak accountManagement function
+  createAccountUrl?(): string; // Keycloak createAccountUrl function
+  loadUserProfile?(): KeycloakPromise<KeycloakProfile, void>; // Keycloak loadUserProfile function
+  loadUserInfo?(): KeycloakPromise<{}, void>; // Keycloak loadUserInfo function
+  subject?: string; // The user id
+  idToken?: string; // The base64 encoded ID token.
+  idTokenParsed?: VueKeycloakTokenParsed; // The parsed id token as a JavaScript object.
+  realmAccess?: KeycloakRoles; // The realm roles associated with the token.
+  resourceAccess?: KeycloakResourceAccess; // The resource roles associated with the token.
+  refreshToken?: string; // The base64 encoded refresh token that can be used to retrieve a new token.
+  refreshTokenParsed?: VueKeycloakTokenParsed; // The parsed refresh token as a JavaScript object.
+  timeSkew?: number; // The estimated time difference between the browser time and the Keycloak server in seconds. This value is just an estimation, but is accurate enough when determining if a token is expired or not.
+  responseMode?: string; // Response mode passed in init (default value is fragment).
+  responseType?: string; // Response type sent to Keycloak with login requests. This is determined based on the flow value used during initialization, but can be overridden by setting this value.
+  hasRealmRole?(role: string): boolean; // Keycloak hasRealmRole function
+  hasResourceRole?(role: string, resource?: string): boolean; // Keycloak hasResourceRole function
+  token?: string; // The base64 encoded token that can be sent in the Authorization header in requests to services
+  tokenParsed?: VueKeycloakTokenParsed; // The parsed token as a JavaScript object
+  keycloak?: KeycloakInstance; // The original keycloak instance 'as is' from keycloak-js adapter
+}
